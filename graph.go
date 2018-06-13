f94de685037547c237f3d638f6f0fdce6d241321
Philipp Drebes <philipp.drebes@gmail.com>
2018-06-13 13:24:22 +0200

    add nodes to graph

--- graph.go
+++ graph.go
@@ -3,53 +3,67 @@ package goji
 import (
 	"github.com/andygrunwald/go-jira"
 	"github.com/awalterschulze/gographviz"
+	"fmt"
 )
 
-func getStatusColor() string {
-	//status := status_field["statusCategory"]["name"].upper()
-	status := "IN PROGRESS"
+const (
+	inward  int = 0
+	outward int = 1
+)
+
+func getStatusColor(issue *jira.Issue) string {
+	status := issue.Fields.Status.Name
 	if status == "IN PROGRESS" {
 		return "yellow"
 	} else if status == "DONE" {
 		return "green"
-		return "white"
 	}
-	return ""
+	return "white"
+}
+
+func addNode(graph *gographviz.Graph, issue *jira.Issue) {
+	if graph.IsNode(issue.Key) == false {
+		attrs := make(map[string]string)
+		attrs[string(gographviz.Shape)] = "box"
+		//attrs[string(gographviz.HREF)] = ""
+		//attrs[string(gographviz.FillColor)] = ""
+		//attrs[string(gographviz.Style)] = "filled"
+
+		graph.AddNode("G", "\""+issue.Key+"\"", attrs)
+	}
 }
 
 func walk(client *jira.Client, issue *jira.Issue, graph *gographviz.Graph) *gographviz.Graph {
 	if graph == nil {
 		graph = gographviz.NewGraph()
-	}
-
-	graph.AddNode("G", issue.Key, nil)
-
-	for _, linkedIssue := range issue.Fields.IssueLinks {
-		linkedIssueObj, _, err := client.Issue.Get(linkedIssue.ID, nil)
-		if err != nil {
-			// todo log err
+		if err := graph.SetDir(true); err != nil {
+			panic(err)
 		}
-		walk(client, linkedIssueObj, graph)
 	}
 
-	//if fields.has_key("issuelinks"){
-	//	for other_link in fields["issuelinks"]{
-	//		result = process_link(fields, issue_key, other_link)
-	//		if result is not None {
-	//			children.append(result[0])
-	//			if result[1] is not None {
-	//				graph.append(result[1])
-	//			}
-	//		}
-	//	}
-	//}
-	//// now construct graph data for all subtasks and links of this issue
-	//for child in (x for x in children if x not in seen) {
-	//	walk(child, graph)
-	//	return graph
-	//}
+	addNode(graph, issue)
 
-	return nil
+	for _, li := range issue.Fields.IssueLinks {
+		fmt.Printf("issue: %s\n", issue.Key)
+
+		var direction int
+		var linkedIssue *jira.Issue
+
+		if li.InwardIssue != nil {
+			direction = inward
+			linkedIssue = li.InwardIssue
+		} else if li.OutwardIssue != nil {
+			direction = outward
+			linkedIssue = li.OutwardIssue
+		}
+
+		fmt.Printf("direction: %d\n", direction)
+		fmt.Printf("linked issue: %s\n", linkedIssue.Key)
+
+		walk(client, linkedIssue, graph)
+	}
+
+	return graph
 }
 
 func BuildGraph(client *jira.Client, issue *jira.Issue) *gographviz.Graph {
